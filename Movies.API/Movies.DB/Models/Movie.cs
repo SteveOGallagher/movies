67055fb9b9cec6e4f739e90ab61eb2269a4f2921
Steve O'Gallagher <steve.ogallagher@cohaesus.co.uk>
2018-05-27 08:56:17 +0100

    Create Movie model in DB project

---
+++ Movies.API/Movies.DB/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿namespace Movies.API.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Movie
+    {
+        [Key]
+        [Column("movie_id")]
+        public int MovieId { get; set; }
+
+        [Column("movie_title")]
+        public string MovieTitle { get; set; }
+
+        [Column("movie_rating")]
+        public double MovieRating { get; set; }
+    }
+}
